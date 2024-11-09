e9d3df4e0c652d0be124181a51f359964687f685
bazliamin90 <bez.aminuddin@gmail.com>
2024-11-09 23:40:43 +0800

    a

--- header.js
+++ header.js
@@ -10,21 +10,19 @@ headerTemplate.innerHTML = `
     }
 	
 	.home {
-		width: 60px;
-		height: 25px;
 		position: fixed;
-		top: 40px;
-		right: -28px;
+		top: 50px;
+		right: -15px;
 		transform: rotate(90deg);
 		z-index: 9;
 		font-size: 10px;
 		color: grey;
-		background-color: #f9f9f9;
+		background-color: #FFCCCB;
 		border-bottom-left-radius: 5px;
 		border-bottom-right-radius: 5px;
 		text-decoration: none;
 		text-align: center;
-		padding: 20px 0 0 0;
+		padding: 10px 5px;
 	}
 	
 	.home:active {
